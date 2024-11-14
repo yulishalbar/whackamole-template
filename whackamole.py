f473c22d6acc38b7fbbca77f81eb5d1c379c76c0
yulishalbar <yulishalbar@ufl.edu>
2024-11-14 17:44:32 -0500

    some changes

--- whackamole.py
+++ whackamole.py
@@ -5,9 +5,10 @@ def main():
     try:
         pygame.init()
         # You can draw the mole with this snippet:
-        # screen.blit(mole_image, mole_image.get_rect(topleft=(x,y)))
+
         mole_image = pygame.image.load("mole.png")
         screen = pygame.display.set_mode((640, 512))
+
         clock = pygame.time.Clock()
         running = True
         while running:
@@ -15,6 +16,7 @@ def main():
                 if event.type == pygame.QUIT:
                     running = False
             screen.fill("light green")
+            screen.blit(mole_image, mole_image.get_rect(topleft=(1, 1)))
             pygame.display.flip()
             clock.tick(60)
     finally:
